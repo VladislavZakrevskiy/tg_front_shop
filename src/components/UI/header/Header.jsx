08be46d644746e31ad4e588d8c0badba0d5131d1
vlad <zakrevskiyvlada@yandex.ru>
2023-02-17 20:11:17 +0400

    modified:   src/App.js 	new file:   src/components/UI/button/button.css 	new file:   src/components/UI/button/button.jsx 	new file:   src/components/UI/header/Header.css 	new file:   src/components/UI/header/Header.jsx 	new file:   src/components/anyFile.jsx 	modified:   src/App.js 	modified:   src/components/UI/header/Header.jsx 	src/hooks/

---
+++ src/components/UI/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+const tg = window.Telegram.WebApp
+
+const Header = () => {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+      
+  return (
+    <div className='header'>
+        <Button onClick = {onClose}>Закрыть</Button>
+        <span className='username'>
+            tg.initDataUnsafe?.user?.username
+        </span>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/anyFile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const anyFile = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default anyFile

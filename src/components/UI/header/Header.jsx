2a697f4842e66545022adaae841d6a162889a5a6
vlad <zakrevskiyvlada@yandex.ru>
2023-02-17 20:18:51 +0400

    modified:   src/App.js 	modified:   src/components/UI/header/Header.jsx

--- src/components/UI/header/Header.jsx
+++ src/components/UI/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTelegram } from '../../../hooks/useTelegram'
+import Button from '../button/button'
 const tg = window.Telegram.WebApp
 
 const Header = () => {

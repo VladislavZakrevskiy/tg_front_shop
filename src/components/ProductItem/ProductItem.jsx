1ccd707556f9282dfcf5a77699d983ea7d3ec5d0
vlad <zakrevskiyvlada@yandex.ru>
2023-02-18 14:13:02 +0400

    modified:   src/components/ProductItem/ProductItem.css 	modified:   src/components/ProductItem/ProductItem.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -13,9 +13,8 @@ const ProductItem = ({product, className, onAdd}) => {
         <div className={'product ' + className}>
             <div className={'img'}/>
             <div className={'title'}>{product.title}</div>
-            <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                <span>Стоимость: <b>{product.price}</b></span>
+                <span>{product.price} Руб.</span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину

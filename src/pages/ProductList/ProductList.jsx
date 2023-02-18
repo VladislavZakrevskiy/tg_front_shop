9083799fb4effbc5936676ecb4a3708027041975
vlad <zakrevskiyvlada@yandex.ru>
2023-02-18 13:25:53 +0400

    modified:   src/pages/ProductList/ProductList.jsx

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -24,7 +24,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId
         }
-        fetch('http://45.140.178.84:8000/web-data', {
+        fetch('http://localhost:8000/web-data', {
         method:'POST',
         headers: {
             'Content-Type':'application/json'

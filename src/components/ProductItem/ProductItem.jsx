14f0074226106ee1b683778576028a8dfd1443f3
vlad <zakrevskiyvlada@yandex.ru>
2023-02-19 17:07:53 +0400

    modified:   src/components/ProductItem/ProductItem.jsx 	modified:   src/pages/ProductList/ProductList.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useImage } from '../../hooks/useImage'
+// import { useImage } from '../../hooks/useImage'
 import Button from '../UI/button/button'
 import './ProductItem.css'
 
@@ -9,12 +9,12 @@ const ProductItem = ({product, className, onAdd}) => {
         onAdd(product)
     }
 
-    const img = useImage(product.id)
+    // const img = useImage(product.id)
 
   return (
     <div>
         <div className={'product ' + className}>
-            <img className='img' src={img} alt='Наш продукт'/>
+            {/* <img className='img' src={img} alt='Наш продукт'/> */}
             <div className={'title'}>{product.title}</div>
             <div className={'price'}>
                 <span>{product.price} Руб.</span>
--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -4,20 +4,20 @@ import ProductItem from '../../components/ProductItem/ProductItem'
 import { useTelegram } from '../../hooks/useTelegram'
 import axios from 'axios'
 
-const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-]
+
 
 const ProductList = () => {
     const {tg, queryId} = useTelegram()
     const [addedItems, setAddedItems] = useState([])
+    const [products, setProducts] = useState([])
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const result = await axios.get('http://localhost:8000/prod/get')
+            setProducts(result.data.rows)
+        }
+        fetchData()
+    }, [])
 
     const onSendData = useCallback(()=>{
         const data = {

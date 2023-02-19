eb7daf31df4d332168880f0d06b77a03188f3d4e
vlad <zakrevskiyvlada@yandex.ru>
2023-02-20 03:00:22 +0400

    modified:   src/pages/ProductList/ProductList.jsx

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -43,7 +43,8 @@ const ProductList = () => {
 
     useEffect(()=>{
         fetchData()
-    }, [sort, fetchData])
+// eslint-disable-next-line
+    }, [sort])
 
     const onSendData = useCallback(()=>{
         const data = {
